b75548c620270b702c3a37fa41f4990709543c49
printix <gamesmen67@gmail.com>
2025-05-12 18:21:25 +1000

    Textures added
     - modified buildinggenerator.js to add building textures
    - added random free building texture

--- BuildingGenerator.js
+++ BuildingGenerator.js
@@ -1,51 +1,90 @@
-//customisable paramaters
+//customisable parameters
 import * as THREE from "three";
 
+// load textures
+const textureLoader = new THREE.TextureLoader();
+const buildingTexture = textureLoader.load('./textures/images.jpg');
+buildingTexture.wrapS = buildingTexture.wrapT = THREE.RepeatWrapping;
+buildingTexture.repeat.set(1, 1);
+
+
 function createCube(w, h, d, color) {
-  var material = new THREE.MeshPhongMaterial();
-  material.color = new THREE.Color(color);
-  material.wireframe = false;
-  var geometry = new THREE.BoxGeometry(w, h, d);
-  var cube = new THREE.Mesh(geometry, material);
+  const texturedMaterial = new THREE.MeshPhongMaterial({
+    map: buildingTexture,
+    color: new THREE.Color(color),
+    wireframe: false,
+  });
+
+  const plainMaterial = new THREE.MeshPhongMaterial({
+    color: new THREE.Color(color),
+    wireframe: false,
+  });
+
+  // textures per building face
+  const materials = [
+    texturedMaterial, // right
+    texturedMaterial, // left
+    plainMaterial,    // top (no texture)
+    plainMaterial,    // bottom (no texture)
+    texturedMaterial, // front
+    texturedMaterial, // back
+  ];
+
+  const geometry = new THREE.BoxGeometry(w, h, d);
+
+  // stop texture from strechiung too much
+  const repeatY = h / 2; // change texture scale
+  const uvs = geometry.attributes.uv;
+  for (let i = 0; i < uvs.count; i++) {
+    const y = uvs.getY(i);
+    uvs.setY(i, y * repeatY);
+  }
+  uvs.needsUpdate = true;
+
+  const cube = new THREE.Mesh(geometry, materials);
   return cube;
 }
+
 
 var MHeight = 20; // height of towers
 var cubes = []; // array of buildings
 const group = new THREE.Group();
 
 function BuildingGenerator(blockW, blockD, xCoord, zCoord) {
-
-  var border = 2;
-  var xTowers = Math.ceil(blockW / 3);
-  var zTowers = Math.ceil(blockD / 3);
-  var Xpos = xCoord-blockW/2+border;
-  var Zpos = zCoord-blockD/2+border;
+  const border = 2;
+  const xTowers = Math.ceil(blockW / 3);
+  const zTowers = Math.ceil(blockD / 3);
+  let Xpos = xCoord - blockW / 2 + border;
+  let Zpos = zCoord - blockD / 2 + border;
 
   for (let i = 0; i < zTowers; i++) {
-    for(let j = 0; j < xTowers; j++) {
-        var BuildingH = Math.random() * (MHeight - 10) + 10; // formula for using Math.random between 2 numbers
-        var BuildingD = Math.random() * 3 +1;
-        var BuildingW = Math.random() * 2 +1;
+    for (let j = 0; j < xTowers; j++) {
+      const BuildingH = Math.random() * (MHeight - 10) + 10;
+      const BuildingD = Math.random() * 3 + 1;
+      const BuildingW = Math.random() * 2 + 1;
 
-        if(Xpos + BuildingW/2 > xCoord + blockW/2 - border){
-            break;
-        }
+      if (Xpos + BuildingW / 2 > xCoord + blockW / 2 - border) {
+        break;
+      }
 
-        cubes[i] = createCube(BuildingW, BuildingH, BuildingD, 0x4d4d4d);
-        cubes[i].position.x = Xpos +BuildingW/2; ;
-        cubes[i].position.y = -(MHeight / 2) + BuildingH / 2;
-        cubes[i].position.z = Zpos +BuildingD/2; ;
+      cubes[i] = createCube(BuildingW, BuildingH, BuildingD, 0x4d4d4d);
+      cubes[i].position.set(
+        Xpos + BuildingW / 2,
+        -(MHeight / 2) + BuildingH / 2,
+        Zpos + BuildingD / 2
+      );
 
-        group.add(cubes[i]);
-        Xpos += BuildingW + border;
-        }
-        Xpos = xCoord-blockW/2+border;
-        Zpos += 2;
+      group.add(cubes[i]);
+      Xpos += BuildingW + border;
     }
-  group.position.y = MHeight / 2;
-  group.position.z=- 2;
-  return group; 
+    Xpos = xCoord - blockW / 2 + border;
+    Zpos += 2;
+  }
 
+  group.position.y = MHeight / 2;
+  group.position.z = -2;
+
+  return group;
 }
+
 export default BuildingGenerator;
